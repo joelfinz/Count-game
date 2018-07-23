90b715b9927a3c582790a25505880685971c57fb
Joel Fintan <joelfintan@gmail.com>
2018-07-23 21:54:46 +0530

    Created Game Objects using Adobe Animate and exported them to the game files

--- scripts/game-es6.js
+++ scripts/game-es6.js
@@ -1,4 +1,18 @@
 //logic of count game.
+
+class NumberedBox extends createjs.Container {
+    constructor(number=0){
+        super();
+        var movieclip = new lib.NumberedBox();
+        this.addChild(movieclip);
+        
+        //random position
+        movieclip.x = Math.random() * 200;
+        movieclip.y = Math.random() * 200;
+    }
+}
+
+
 class Game {
     constructor() {
         console.log(`Welcome to the game. Version: ${this.version()}`);
@@ -6,10 +20,18 @@ class Game {
         this.canvas = document.getElementById("game-canvas");
         this.stage = new createjs.Stage(this.canvas);
 
+        window.debugStage = this.stage;
+
         createjs.Ticker.setFPS(60);
 
         //keep redrawing the stage.
         createjs.Ticker.on("tick", this.stage);
+        
+        //background
+        this.stage.addChild(new lib.Background());
+
+        //testing code
+        this.stage.addChild(new NumberedBox(88));
     }
     version() {
         return '1.0.0';
